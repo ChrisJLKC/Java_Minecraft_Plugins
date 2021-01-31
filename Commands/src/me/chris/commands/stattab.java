bf20823d637a1cee6a0f09d21999a1a836ed9746
Christopher Jenner <49998052+ChrisJLKC@users.noreply.github.com>
2021-01-31 15:01:34 +0000

    Fixed autofill problem

--- Commands/src/me/chris/commands/stattab.java
+++ Commands/src/me/chris/commands/stattab.java
@@ -13,17 +13,20 @@ public class stattab implements TabCompleter {
 	
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 		if (arguments.isEmpty()) {
-			arguments.add("logins"); arguments.add("playerKills"); arguments.add("deaths"); arguments.add("mobKills");
+			arguments.add("logins"); arguments.add("playerKills"); 
+			arguments.add("deaths"); arguments.add("mobKills");
 		}
 		
 		List<String> result = new ArrayList<String>();
 		
 		if (args.length == 1) {
 			for (String a : arguments) {
-				if (a.toLowerCase().startsWith(args[0].toLowerCase())) result.add(a);
+				if (a.toLowerCase().startsWith(args[0].toLowerCase())) 
+					result.add(a);
 			
-				return result;
 			}
+			
+			return result;
 		}
 		
 		return arguments;
